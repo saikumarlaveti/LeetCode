b7fbf085e18eec994a23abd677df8cc5f5b7dd91
saikumar <saikumarlaveti5061@gmail.com>
2025-03-15 22:49:15 +0530

    FizzBizz

---
+++ src/Strings/FizzBizz_412.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBizz_412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> arr = new ArrayList<>();
+        for(int i = 1;i<=n;i++){
+            if(i % 3 == 0 && i%5 == 0){
+                arr.add( "FizzBuzz");
+            }
+            else if(i%3==0){
+                arr.add("Fizz");
+            }
+            else if(i%5==0){
+                arr.add("Buzz");
+            }
+            else{
+                arr.add(String.valueOf(i));
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        FizzBizz_412 fb = new FizzBizz_412();
+       List<String> res =  fb.fizzBuzz(5);
+        System.out.println(res);
+    }
+}
