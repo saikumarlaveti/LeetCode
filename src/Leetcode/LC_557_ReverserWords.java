6e79bb60a34077eb939eb26bf277ce55ca4d04ba
saikumar <saikumarlaveti5061@gmail.com>
2025-04-20 23:06:16 +0530

    ReverseWords

---
+++ src/Leetcode/LC_557_ReverserWords.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Arrays;
+
+public class LC_557_ReverserWords {
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder(word);
+            result.append(sb.reverse()).append(" ");
+        }
+
+        return result.toString().trim(); // remove the trailing space
+    }
+    public static void main(String[] args) {
+    String name = "Let's take LeetCode contest";
+        System.out.println(reverseWords(name));
+
+    }
+}
